8967e13b8cdff5ac4d4dea3b7eb25cca4eb39b72
Merlyin <focastefan13@gmail.com>
2024-08-05 21:17:28 +0300

    now please
    work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
-import React, { Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from '../Home/Home';
 import css from './App.module.css';
-// Import komponentów asynchronicznie
+// Import async components
 const Movies = React.lazy(() => import('../Movies/Movies'));
 const MovieDetails = React.lazy(() => import('../MovieDetails/MovieDetails'));
 const Cast = React.lazy(() => import('../Cast/Cast'));
@@ -21,9 +21,10 @@ export const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId/*" element={<MovieDetails />} />
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />} >
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Routes>
         </Suspense>
       </div>
